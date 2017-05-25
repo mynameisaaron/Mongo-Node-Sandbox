df238578b90ce85da1817d050e8fda51f8b2ca26
Aaron Brightman <aaronbrightman1@gmail.com>
2017-05-25 10:43:11 -0400

    beforeEach in a synchronous order with done parameter and drop call back function

--- test/test_helper.js
+++ test/test_helper.js
@@ -6,7 +6,18 @@ mongoose.connection
 .on('error', (error)=>{console.warn('Warning', error)});
 
 
-//This is the hook added.  Will Run beforeEach test
-beforeEach(()=>{
-    mongoose.connection.collections.users.drop();
+//Mocha is Asynchronous, needs to run this script to drop the data table -before starting the mocha test.
+//ie, we need a guarantee that it will drop before the test runs.
+/// use the mocha's .done() call back function to guarantee the order of operations
+
+//1).drop will take a callback function that will file once its done droping the table
+
+//2)lots of mocha function get call back with a done parameter 
+
+
+beforeEach((done)=>{
+    mongoose.connection.collections.users.drop(()=>{
+        // Ready to run the next test
+        done();
+    });
 });
