43f58d7e6f6161d272499c532a96533c529d3b6b
Aaron Brightman <aaronbrightman1@gmail.com>
2017-05-25 11:09:12 -0400

    Set mongoose to use newer ES6 Promise library

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
+//telling mongoose that we want to use the ES6 to make a promise, just like node's global promise
+////deprication message removed
+mongoose.Promise = global.Promise;
+
 mongoose.connect('mongodb://localhost/user_test');
 mongoose.connection
-.once('open', ()=>{console.log('good to go!')})
+.once('open', ()=>{})
 .on('error', (error)=>{console.warn('Warning', error)});
 
 
