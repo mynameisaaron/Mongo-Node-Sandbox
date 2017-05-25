d908ecc70e9de08ad4489ff5ae3841071bfab598
Aaron Brightman <aaronbrightman1@gmail.com>
2017-05-25 11:01:42 -0400

    using US6 promises to create syncronous order or events

--- test/create_test.js
+++ test/create_test.js
@@ -3,11 +3,18 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Creating Records',()=>{
-    it('saves a user',()=>{
+    it('saves a user',(done)=>{
 
         const joe = new User({name:'Joe Six-Pack'});
 
-        joe.save();
+        //using US6 promises to create syncronous order or events
+        joe.save()
+        .then(()=>{
+            assert(!joe.isNew);
+            //in Mocha done() is available to every it() block and beforeEach() block
+            done();
+        })
+        .catch(()=>{console.log('there was a problem')});
 
     });
 });
