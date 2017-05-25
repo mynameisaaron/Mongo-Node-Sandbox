12edaef82a10d388a19da273ec529faa7507bf6b
Aaron Brightman <aaronbrightman1@gmail.com>
2017-05-25 12:48:18 -0400

    Finding One Record with a Particular _id

--- test/reading_test.js
+++ test/reading_test.js
@@ -1,26 +1,41 @@
 const assert = require('assert');
 const User = require('../src/user');
 
-describe('Reading users out of the database',()=>{
-let joe;
+describe('Reading users out of the database', () => {
+    let joe;
 
-beforeEach((done)=>{
-    joe = new User({name:'Joe'});
-    joe.save()
-    .then(()=>{done();})
-});
+    beforeEach((done) => {
+        joe = new User({ name: 'Joe' });
+        joe.save()
+            .then(() => { done(); })
+    });
 
-    it('finds all users with a name of joe',(done)=>{
-        
-        
-        User.find({name:"Joe"})
-        .then((UsersFound)=>{
-            
-assert(joe._id.toString() === UsersFound[0]._id.toString());
+    it('finds all users with a name of joe', (done) => {
 
-            done();
-        });
+
+        User.find({ name: "Joe" })
+            .then((UsersFound) => {
+
+                assert(joe._id.toString() === UsersFound[0]._id.toString());
+
+                done();
+            });
 
     })
+
+    it('find one user with a particular _id',(done)=>{
+
+        User.findOne({_id:joe._id})
+        .then((SingleUser)=>{
+            
+            assert(SingleUser.name === "Joe")
+            //GOTCHYA, Carefull to execute done inside the promise
+            done();
+        });
+        
+
+        
+
+    });
 
 });
