a8a87e47f7bf1ceeeddbf741e18a09dd99f715f2
Aaron Brightman <aaronbrightman1@gmail.com>
2017-05-27 13:13:34 -0400

    Add Subdocument to Existing Model-Object record from database

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -3,6 +3,8 @@ const User = require('../src/user');
 
 describe('Sub-Schema of User Model', () => {
 
+
+
     it('Saved User Object with Sub-schema', (done) => {
         const joe = new User({
             name: 'Joe1',
@@ -17,4 +19,36 @@ describe('Sub-Schema of User Model', () => {
         done();
     });
 
+    it('Add Subdocument to Existing Model-Object record from database', (done) => {
+
+
+
+        var JoeRogan = new User({ name: 'Joe Rogan' });
+        JoeRogan.save()
+            .then(() => User.findOne({ name: 'Joe Rogan' }))
+            .then((_joerogan) => {
+
+                _joerogan.posts = [];
+                _joerogan.posts.push({ title: 'here it is' });
+
+                //the teacher stops nesting and returns the save()
+               return _joerogan.save();
+            })
+            .then(() => User.findOne({ name: 'Joe Rogan' }))
+            .then((_User) => {
+                assert(_User.posts[0].title === 'here it is');
+                done();
+            })
+
+    });
+
+
+
+
+
 });
+
+
+
+
+
--- test/update_test.js
+++ test/update_test.js
@@ -7,6 +7,8 @@ const User = require('../src/user');
 describe('Updateing a user', () => {
     let joe;
 
+
+
     beforeEach((done) => {
         joe = new User({ name: 'Joe', postCount: 0 });
         joe.save()
@@ -14,90 +16,90 @@ describe('Updateing a user', () => {
     });
 
 
-   
+
 
     it(' \'Set and Save\' method on the object', (done) => {
         joe.set('name', 'Aaron');
         joe.save()
             .then(() => User.find({}))
-            .then( (userArray) => {
-                   
-                    assert(userArray.length === 2);
-                    assert(userArray[1].name === 'Aaron');
-                    
-                    done();
+            .then((userArray) => {
 
-                });
+                assert(userArray.length === 1);
+                assert(userArray[0].name === 'Aaron');
+
+                done();
+
+            });
 
     });
 
-//For some 
+    //For some 
 
-    it('update function on the object', (done)=>{
-        joe.update({name:"Brightman"})
-        .then(()=>User.find({}))
-        .then((userArray)=>{
-             assert(userArray.length === 1);
-             assert(userArray[0].name === 'Brightman');
+    it('update function on the object', (done) => {
+        joe.update({ name: "Brightman" })
+            .then(() => User.find({}))
+            .then((userArray) => {
+                assert(userArray.length === 1);
+                assert(userArray[0].name === 'Brightman');
 
-            
-            done();
-        });
+
+                done();
+            });
     });
 
-    it('static method find all record with a match and Update',(done)=>{
+    it('static method find all record with a match and Update', (done) => {
 
         //static  update takes two paramters, one is the filter then the givin data
-        User.update({name:"Joe"},{name:"Alex"})
-        .then(()=>User.find({}))
-        .then((userArray)=>{
-            assert(userArray.length === 1);
-            assert(userArray[0].name === 'Alex');
-            done();
-        })
+        User.update({ name: "Joe" }, { name: "Alex" })
+            .then(() => User.find({}))
+            .then((userArray) => {
+                assert(userArray.length === 1);
+                assert(userArray[0].name === 'Alex');
+                done();
+            })
 
     });
 
 
-    it('static method find single record with a match and Update',(done)=>{
+    it('static method find single record with a match and Update', (done) => {
 
         //static  update takes two paramters, one is the filter then the givin data
-        User.findOneAndUpdate({name:"Joe"},{name:"Erin"})
-        .then(()=>User.find({}))
-        .then((userArray)=>{
-            assert(userArray.length === 1);
-            assert(userArray[0].name === 'Erin');
-            done();
-        })
+        User.findOneAndUpdate({ name: "Joe" }, { name: "Erin" })
+            .then(() => User.find({}))
+            .then((userArray) => {
+                assert(userArray.length === 1);
+                assert(userArray[0].name === 'Erin');
+                done();
+            })
 
     });
 
-    it('static method, find one match by id and update',(done)=>{
+    it('static method, find one match by id and update', (done) => {
 
-        User.findByIdAndUpdate(joe._id,{name:'Rudolph'})
-        .then(()=>User.find({}))
-        .then((userArray)=>{
-            assert(userArray.length === 1);
-            assert(userArray[0].name === 'Rudolph');
-            done();
-        });
+        User.findByIdAndUpdate(joe._id, { name: 'Rudolph' })
+            .then(() => User.find({}))
+            .then((userArray) => {
+                assert(userArray.length === 1);
+                assert(userArray[0].name === 'Rudolph');
+                done();
+            });
+
+    });
+
+
+    it('Increment postCount by one', (done) => {
+
+        User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+            .then(() => User.findOne({ name: 'Joe' }))
+            .then((userJoe) => {
+                assert(userJoe.postCount === 1);
+                done();
+            })
+
 
     });
 
-
-    it('Increment postCount by one',(done)=>{
-
-        User.update({name:'Joe'}, {$inc: { postCount : 1}})
-        .then(() => User.findOne({name:'Joe'}))
-        .then((userJoe)=>{
-            assert(userJoe.postCount === 1);
-            done();
-        })
-       
-            
-        });
-
-    });
+});
 
 
 
