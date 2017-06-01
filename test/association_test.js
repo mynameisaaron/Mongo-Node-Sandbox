59465006231bd4e790447ac108c36c5f7531e98d
Aaron Brightman <aaronbrightman1@gmail.com>
2017-06-01 13:17:20 -0400

    Populate the Whole Tree of the record, ie populate a nested record

--- test/association_test.js
+++ test/association_test.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const assert = require('assert');
+
 const User = require('../src/user');
 const Comment = require('../src/comment');
 const BlogPost = require('../src/blogpost');
@@ -22,11 +24,39 @@ let joe,blogpost,comment;
 
     });
 
-    it.only('Saves a relation between a user and a blogpost',(done)=>{
+    it('Saves a relation between a user and a blogpost',(done)=>{
         User.findOne({name:'Joe'})
         .then((user)=>{
-            console.log(user);
+            assert(joe.name === 'Joe')
             done();
         })
     });
+
+    it('Saves a relation between a user and a blogpost (with the populate modifier)',(done)=>{
+        User.findOne({name:'Joe'}).populate('blogPosts')
+        .then((user)=>{
+            assert(user.blogPosts[0].title === 'Howdy');
+            done();
+        })
+    });
+
+//Here, populate also can take a configuration object
+    it.only('Populate the Whole Tree of the record, ie populate a nested record',(done)=>{
+        User.findOne({name:'Joe'})
+        .populate({
+            path:'blogPosts',
+            populate :
+            {
+                path: 'comments',
+                model:'comment'
+            }
+        })
+        .then((user)=>{
+            console.log(user.blogPosts[0]);
+            done();
+        
+    });
+        
+    });
+
 });
